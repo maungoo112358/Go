a729de4057283e3f8729a3ea3e8e8288e2b254d9
MaungOo <83910863+maungoo112358@users.noreply.github.com>
2025-06-17 15:17:50 +0700

    Add Url format

--- download/download.go
+++ download/download.go
@@ -1,22 +1,39 @@
 package download
 
 import (
+	"fmt"
+	"net/url"
 	"os"
 	"os/exec"
 	"path/filepath"
 )
 
 func DownloadYoutubeAsMp3(videoURL string) error {
+	cleanURL, err := CleanYoutubeURL(videoURL)
+	if err != nil {
+		return err
+	}
 
 	outputDir := EnsureOutputFolder()
 
-	cmd := exec.Command("./yt-dlp.exe", "-x", "--audio-format", "mp3", "-o", filepath.Join(outputDir, "%(title)s.%(ext)s"), videoURL)
-
+	cmd := exec.Command("./yt-dlp.exe", "-x", "--audio-format", "mp3", "-o", filepath.Join(outputDir, "%(title)s.%(ext)s"), cleanURL)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
 	return cmd.Run()
+}
 
+func CleanYoutubeURL(rawURL string) (string, error) {
+	u, err := url.Parse(rawURL)
+	if err != nil {
+		return "", err
+	}
+	q := u.Query()
+	videoID := q.Get("v")
+	if videoID == "" {
+		return "", fmt.Errorf("missing v parameter in URL")
+	}
+	return "https://www.youtube.com/watch?v=" + videoID, nil
 }
 
 func getDesktopPath() string {
